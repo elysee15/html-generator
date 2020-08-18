fe8d310192cdd691b9e8ce8ad1251a74f96d38db
elyseebleu <elyseebleu1@gmail.com>
2020-08-18 22:59:01 +0000

    Feat: add searching by paragraph

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ let index = [];
 let chapterValue = [];
 let paragraph = [];
 
-
 function preload(){
     txt = loadStrings("les-chiendent.txt");
 }
@@ -11,17 +10,26 @@ function preload(){
 function setup(){
     noCanvas();
     // createDiv(join(txt, '</br>'));
+
     txt.forEach((value, i) => {
-        if (value.match("CHAPITRE")){
-            
+        if (value.match("Paragraphe")){
             index.push(i)
             chapterValue.push(value);
         }
     });
     // createElement('h3', txt[index[0]]);
     for (let i=0, size = index.length; i<size; i++){
-        paragraph.push(txt.slice(index[i]+1, index[i+1]).join('<br/>'));
+        paragraph
+        .push(txt.slice(index[i]+1, index[i+1])
+        .join('<br/>'));
     }
+    chapterValue.forEach((element, i) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', i+1);
+        const text = document.createTextNode(element);
+        option.appendChild(text);
+        document.getElementById('chapter').appendChild(option);
+    });
 }
 
 
